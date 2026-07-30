dadac2ce62b1d097839105a5b4a27657f7c0b0b5
his188 <ug51blue@gmail.com>
2026-07-30 16:05:06 +0100

    fixing

--- src/search.h
+++ src/search.h
@@ -346,7 +346,7 @@ class Worker {
    private:
     bool iterative_deepening();
 
-    rootCorrectionUncertainty = 0;
+    int rootCorrectionUncertainty = 0;
 
     void do_move(Position& pos, const Move move, StateInfo& st, Stack* const ss);
     void
